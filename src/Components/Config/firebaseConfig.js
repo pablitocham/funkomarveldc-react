b157a02015712e712215ea5d92c6b4894b28eae6
Juan Pablo Chamorro <jpablocham@gmail.com>
2024-10-31 11:03:15 -0300

    ultimos-cambios

--- src/Components/Config/firebaseConfig.js
+++ src/Components/Config/firebaseConfig.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDh2EIQ-w8bbUJJArI5bl8RQRTcNUt4mZo",
-  authDomain: "funkos-d2411.firebaseapp.com",
-  projectId: "funkos-d2411",
-  storageBucket: "funkos-d2411.appspot.com",
-  messagingSenderId: "493509665334",
-  appId: "1:493509665334:web:3d074b03043d9e04416e97"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
--- src/Components/Pages/SuperHero.jsx
+++ src/Components/Pages/SuperHero.jsx
@@ -1,11 +1,76 @@
-import React from "react";
-import '../Pages/SuperHero.css'
-const SuperHero = () =>{
-    return(
-        <div className="Super">
-            <h1> Bienvenidos</h1>
-            <h2>Sección en construccion</h2>
+import { useState } from 'react'
+import './SuperHero.css'
+
+const SuperHero = () => {
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    const [consult, setConsult] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (name.trim() === '') {
+            Swal.fire({
+                title: 'Ingrese su nombre',
+                icon: "info"
+            })
+            return
+        }
+
+        if (isNaN(phone.trim()) || phone.trim() === '') {
+            Swal.fire({
+                title: 'Ingrese su número de contacto',
+                icon: "info"
+            })
+            return
+        }
+
+        if (email.trim() === '' || !email.includes('@')) {
+            Swal.fire({
+                title: 'Ingrese email valido',
+                icon: "info"
+            })
+            return
+        }
+        if (consult.trim() === '') {
+            Swal.fire({
+                title: 'Ingrese su consulta',
+                icon: "info"
+            })
+            return
+        }
+    
+        console.log('Consulta Enviada')
+        console.log('Nombre:', name)
+        console.log('Cel:', phone)
+        console.log('Email:', email)
+        console.log('Consulta:', consult)
+        Swal.fire({
+            title: 'Consulta enviada con éxito',
+            text: 'Dentro de las 24hs le responderemos',
+            icon: "success"
+        })
+
+        setName('')
+        setPhone('')
+        setEmail('')
+        setConsult('')
+    }
+  
+
+    return (
+        <div>
+            <h1>Contactanos </h1>
+            <form className='Super' onSubmit={handleSubmit}>
+                <input type='text' placeholder='Ingresa tu nombre' value={name} onChange={(e) => setName(e.target.value)} />
+                <input type='text' placeholder='Ingresa tu cel' value={phone} onChange={(e) => setPhone(e.target.value)} />
+                <input type='text' placeholder='Ingresa tu e-mail' value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type='text' placeholder='Ingresa tu consulta' value={consult} onChange={(e) => setConsult(e.target.value)} />
+                <button className='Contacto' type='submit'> Enviar Consulta</button>
+            </form>
+
         </div>
     )
 }
-export default SuperHero
+    export default SuperHero
