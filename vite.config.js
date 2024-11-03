815e6c54760d4b0f1fe8b3d72f07180edb17493f
Juan Pablo Chamorro <jpablocham@gmail.com>
2024-11-03 11:43:30 -0300

    build

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/funkomarveldc-react/', 
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-  },
 })
